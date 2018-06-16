a0c012ae94b2401177002b200be5a9e0b25f5074
Daniel Boline <ddboline@gmail.com>
2018-06-16 14:25:09 -0400

    I hate tagged versions...

--- setup.py
+++ setup.py
@@ -24,12 +24,12 @@ with open('README.rst') as f:
 # especially for end-users (non-developers) who use pip to install hangups.
 install_requires = [
     'ConfigArgParse>=0.11.0',
-    'aiohttp>=1.3,<3',
-    'appdirs>=1.4,<1.5',
+    'aiohttp>=1.3',
+    'appdirs>=1.4',
     'readlike>=0.1.2',
-    'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
+    'requests>=2.6.0',  # uses semantic versioning (after 2.6)
     'ReParser>=1.4.3',
-    'protobuf>=3.1.0,<3.2.0',
+    'protobuf>=3.1.0',
     'urwid>=1.3.1',
     'MechanicalSoup>=0.6.0',
 ]
